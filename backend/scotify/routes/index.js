760cee02a83a94acd0a94bcc5c9b03aab45aa894
George McCarron <hello@georgemccarron.com>
2018-10-25 17:11:03 +0100

    adding translate route

--- backend/scotify/routes/index.js
+++ backend/scotify/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
+router.get('/translate/:id', (req, res) => {
+  console.log('hit translate');
+  //res.render('index', {'url': url});
+  res.send('url: ' + req.params.id);
+});
 module.exports = router;
