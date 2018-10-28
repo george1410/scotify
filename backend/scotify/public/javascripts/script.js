9af6b3dcff3da7dd748037a03611068302a40b23
George McCarron <hello@georgemccarron.com>
2018-10-28 01:23:53 +0100

    changed the use-case

---
+++ backend/scotify/public/javascripts/script.js
@@ -0,0 +1,15 @@
+$(function () {
+    $('form').submit(function (e) { 
+        e.preventDefault();
+        var inputText = $('#input-text').val();
+
+        data = {
+            text: inputText
+        };
+
+        $.getJSON('/translate', data, (data) => {
+                console.log(data);
+            }
+        );
+    });
+});
--- backend/scotify/routes/index.js
+++ backend/scotify/routes/index.js
@@ -1,13 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.post('/translate', (req, res, next) => {
-  console.log(req.body);
+router.get('/translate', (req, res, next) => {
+  //TODO: business logic goes here.
+
+  //Return the JSON array of data back to the client.
+  res.send(data);
 });
 
 module.exports = router;
